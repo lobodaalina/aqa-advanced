de9f452078739b7e2d7fddfba71d6de95f7141f8
Alina Loboda <lobodaalina99@gmail.com>
2025-06-16 10:16:14 +0200

    Fixed tasks 2 and 3

--- hw_6/task2.js
+++ hw_6/task2.js
@@ -6,10 +6,7 @@
 (чи є особа дорослою) в консоль для кожного випадку.*/
 
 function isAdult(age) {
-    if (age >=18) {
-        return 'true'
-    } else {
-        return 'false'}
-    }   
+   return age >=18
+}   
 console.log(isAdult(25))
 console.log(isAdult(15))
--- hw_6/task3.js
+++ hw_6/task3.js
@@ -10,12 +10,15 @@ ordered - кількість одиниць товару в замовленні
 
 function checkOrder (available, ordered) {
     if (available < ordered) {
-        return 'Your order is too large, we don’t have enough goods.'
-    } else if (ordered === 0) {
-        return 'Your order is empty'
-    } else {
-        return 'Your order is accepted'
-    }
+        return 'Your order is too large, we don’t have enough goods.';
+    } 
+    
+    if (ordered === 0) {
+        return'Your order is empty';
+    } 
+        
+    return 'Your order is accepted';
+    
 }
 
-console.log(checkOrder(30, 0))
+console.log(checkOrder(30, 31))
