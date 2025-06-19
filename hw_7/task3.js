bc748224d2004a8b875b8a0c2f71ec0a5b5ccd90
Alina Loboda <lobodaalina99@gmail.com>
2025-06-19 15:30:19 +0200

    Added homework 7

---
+++ hw_7/task3.js
@@ -0,0 +1,38 @@
+/* Створіть функцію divide, яка приймає два параметри: numerator і denominator та повертає як результат 
+виконання число отримане від ділення.
+У функції треба поділити numerator на denominator і повернути результат.
+Додайте валідацію в функції. У разі,
+якщо denominator дорівнює 0
+або хоча б один з аргументів не є числом, викиньте помилку з інформативним повідомленням,
+Викличте функцію divide з різними значеннями numerator і denominator, включаючи випадок, коли denominator 
+дорівнює 0 або один з аргументів не є числом.
+Огорніть кожен окремий виклик функції divide в try…catch.Використовуючи блок finally, виведіть повідомлення 
+"Робота завершена" в консоль, навіть якщо помилка виникла або не виникла. */
+
+function divide(numerator, denominator) {
+    if (numerator === 0 || denominator === 0) {
+        throw new TypeError("Ділення на нуль недопустиме!");
+    }
+    if (isNaN(numerator) || isNaN(denominator)) {
+        throw new TypeError("Це не цифра!")
+    }
+   return numerator / denominator;
+}
+
+console.log(divide(10,5))
+
+try {
+  divide('fkff', 1);
+} catch (e) {
+  console.error(e);
+}
+
+try {
+ divide(0, 200);
+} catch (e) {
+  console.error(e);
+}
+
+finally {
+    console.log('Робота завершена')
+}
