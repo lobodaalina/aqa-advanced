feaae033ecc6e4d992edf2d2c06ba9722afc0104
Alina Loboda <lobodaalina99@gmail.com>
2025-07-23 17:34:34 +0200

    Added homework 14

---
+++ hw_14/get1.test.js
@@ -0,0 +1,11 @@
+const axios = require('axios')
+
+test("Get all posts status is 200", async () => {
+    const response = await axios.get('https://jsonplaceholder.typicode.com/posts', {
+        headers: {
+            'Content-Type': 'application/json',
+        }
+    });
+    console.log(response.data);
+    expect(response.status).toEqual(200);
+})
