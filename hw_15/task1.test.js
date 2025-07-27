eac9b06b1553c89a9a97eff6289a946b7e5378e7
Alina Loboda <lobodaalina99@gmail.com>
2025-07-27 17:50:21 +0200

    Added homework 15

---
+++ hw_15/task1.test.js
@@ -0,0 +1,10 @@
+const axios = require('axios');
+
+async function fetchData() {
+    const res = await axios.get('https://httpbin.org/get22');
+    return res.data;
+}
+
+test('Wrong url with error 404', () => {
+  expect(() => fetchData()).rejects.toThrow();
+});
